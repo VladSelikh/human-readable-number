2a570abb9c6f91513f887a076c6f7ff5d169161e
VladSelikh <vladikh2405@gmail.com>
2020-10-20 23:43:22 +0300

    feat: add hundreds

--- src/index.js
+++ src/index.js
@@ -6,4 +6,33 @@ module.exports = function toReadable (number) {
     if (a===0 && b===0 && c===0) {
       return 'zero';
     }
+    switch (a) {
+        case (1):
+          humReadNum = 'one hundred';
+          break;
+        case (2):
+          humReadNum = 'two hundred';
+          break;
+          case (3):
+          humReadNum = 'three hundred';
+          break;
+          case (4):
+          humReadNum = 'four hundred';
+          break;
+          case (5):
+          humReadNum = 'five hundred';
+          break;
+           case (6):
+          humReadNum = 'six hundred';
+          break;
+           case (7):
+          humReadNum = 'seven hundred';
+          break;
+           case (8):
+          humReadNum = 'eight hundred';
+          break;
+           case (9):
+          humReadNum = 'nine hundred';
+          break;
+      }
 }

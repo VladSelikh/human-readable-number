55b9495aa2ae39e4c0a4309fc49f6d4d25b076ad
VladSelikh <vladikh2405@gmail.com>
2020-10-20 23:42:08 +0300

    init: start human readable task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function toReadable (number) {
-  
+    let c = number % 10;
+    let b = ((number - c) % 100)/10;
+    let a = ((number - c - 10 * b) % 1000)/100;
+    let humReadNum = "";
+    if (a===0 && b===0 && c===0) {
+      return 'zero';
+    }
 }
